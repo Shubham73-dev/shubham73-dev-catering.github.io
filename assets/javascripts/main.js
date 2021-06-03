9a00a77ceb9548d93b3f611e25047f334a17f0b7
Shubham73-dev <79575532+Shubham73-dev@users.noreply.github.com>
2021-06-04 02:47:40 +0530

    Add files via upload

---
+++ assets/javascripts/main.js
@@ -0,0 +1,12 @@
+window.addEventListener("load",initEvent);
+function initEvent(){
+    const toTop=document.querySelector(".to-top");
+window.addEventListener("scroll",()=>{
+    if(window.pageYOffset>100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
+}
